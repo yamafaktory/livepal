2ce933599e234505d0b8e15b05c7cda8f6414795
yamafaktory <yamafaktory@gmail.com>
2014-09-30 14:44:29 +0200

    Update

--- app/src/js/livepal-ui.js
+++ app/src/js/livepal-ui.js
@@ -5,16 +5,9 @@ Polymer('livepal-ui', {
   },
   // Set observers
   observe: {
-    '$.name.inputValue': 'checkName'
+    //'$.username.inputValue': 'test'
   },
   // Methods
-  checkName: function () {
-    console.log(this.$.name.inputValue.length);
-    if(this.$.name.inputValue.length < 5){
-      this.$.name.classList.add('invalid');
-      console.log('not valid');
-    }
-  },
   switch: function (event, detail, sender) {
     this.$.main.selected = sender.dataset.target;
   }
