93b38f187f7cecb8a1de7e808e2f112145275828
yamafaktory <yamafaktory@gmail.com>
2014-10-01 00:56:36 +0200

    Update

--- app/src/js/livepal-ui.js
+++ app/src/js/livepal-ui.js
@@ -1,13 +1,17 @@
 Polymer('livepal-ui', {
   // Create
   created: function () {
-    this.disableForm = true;
+    this.test = 'test';
   },
   // Set observers
   observe: {
     //'$.username.inputValue': 'test'
   },
   // Methods
+  isInvalid: function (string) {
+    let usernameRegex = /^\S{5,20}$/;
+    return !usernameRegex.test(string);
+  },
   switch: function (event, detail, sender) {
     this.$.main.selected = sender.dataset.target;
   }
