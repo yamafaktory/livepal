b488e6ac79b2bf2d8effaa43c78524c8c9fae751
yamafaktory <yamafaktory@gmail.com>
2014-10-02 23:43:45 +0200

    Playing with gifshot

--- app/src/js/livepal-ui.js
+++ app/src/js/livepal-ui.js
@@ -12,6 +12,15 @@ Polymer('livepal-ui', {
     let usernameRegex = /^\S{5,20}$/;
     return !usernameRegex.test(string);
   },
+  selfie: function () {
+    this.gifshot.createGIF({}, obj => {
+      if(!obj.error) {
+        let image = obj.image;
+        let animatedImage = this.$.gifie;
+        animatedImage.src = image;
+      }
+    });
+  },
   switch: function (event, detail, sender) {
     this.$.main.selected = sender.dataset.target;
   }
--- app/src/js/livepal.js
+++ app/src/js/livepal.js
@@ -1,9 +1,15 @@
 'use strict';
 
+// Import gifshot
+import gifshot from 'gifshot/build/gifshot.min.js';
+
 // Import components as prototypes
 import './livepal-ui';
 
 // When all components are ready, do stuff
 window.addEventListener('polymer-ready', event => {
-  
+  //  Select livepal-ui
+  var livepal = document.querySelector('livepal-ui');
+  //  Attach gifshot as a property on livepal-ui
+  livepal.gifshot = gifshot;
 });
